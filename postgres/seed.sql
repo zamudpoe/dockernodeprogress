4f51816d9cd3b87489392581ed48a91bb4e92f39
Ing.Engelbert Zamudio Ponzzi <engel_zamudio@icloud.com>
2017-04-21 16:53:53 -0500

    Persistencia de base de datos postgress

---
+++ postgres/seed.sql
@@ -0,0 +1,9 @@
+CREATE TABLE votes (
+  id integer PRIMARY KEY,
+  option_name varchar(20),
+  number_of_votes integer
+);
+
+INSERT INTO votes (id, option_name, number_of_votes) VALUES (1, 'tacos', 10);
+INSERT INTO votes (id, option_name, number_of_votes) VALUES (2, 'sandwiches', 7);
+INSERT INTO votes (id, option_name, number_of_votes) VALUES (3, 'otros...', 32)
--- src/server.js
+++ src/server.js
@@ -2,16 +2,47 @@
 
 const express  = require('express'); 
 const app      = express(); 
+let bodyParser = require('body-parser');   /*  */
+let ejs        = require('ejs');           /* plantillas ejs, parecido a pug */ 
+let pg         = require('pg');            /* postgress */
 
-let bodyParser = require('body-parser'); 
-let ejs        = require('ejs'); 
-let pg         = require('pg'); 
+/*
+  Con el comando 
+    docker network inspect bridge 
+  obtenemos el gateway para que se conecte nuestros contenedores node-app y node-db  
+ 
+  "Gateway": "172.17.0.1" 
+*/ 
+let client = new pg.Client('postgres://postgres@172.17.0.1:9000/postgres');
 
 let votes = {
   sandwiches : 0,
   tacos      : 0,
   otros      : 0
-};
+}; 
+
+client.connect( function(err) { 
+  if (err) throw err;
+
+  /* Ejecutamos un query en nuestra base de datos. */
+  client.query("SELECT * FROM votes", function (err, result) {
+    /*if (err) console.log(err);*/
+    if (err) throw err;
+
+    /* Actualizamos nuestro objeto votes con el resultado de la base de datos */
+    votes.sandwiches = result.rows[0].number_of_votes;
+    votes.tacos      = result.rows[1].number_of_votes;
+    votes.otros      = result.rows[2].number_of_votes;
+ 
+    console.log("\n\tTacos: " + votes.tacos + "\n\tSandwiches: " + votes.sandwiches + "\n\tOtros: " + votes.otros);
+  }); 
+  
+  // disconnect the client 
+  /*client.end( function (err) {
+   if (err) throw err;
+  });
+ */
+});
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -27,8 +58,8 @@ app.get('/', function (req, res) {
   });
 });
 
-/* Funcionalidad para manejo de votaciones */
-app.post("/vote", urlencodedParser, function(req, res){
+/* Funcionalidad para manejo de votaciones con persitencia solo en MEMORIA RAM */
+/* app.post("/vote", urlencodedParser, function(req, res){
   let vote = req.body.yourVote;
   if (vote === 'sandwiches') {
     votes.sandwiches = votes.sandwiches + 1;
@@ -42,9 +73,34 @@ app.post("/vote", urlencodedParser, function(req, res){
   } else {
     console.log( 'Algo salio mal: ' + vote );
   }
-  /* nos aseguramos de redirigir a la ruta por default */
+   // nos aseguramos de redirigir a la ruta por default 
   res.redirect('/');
-}); 
+}); */
+
+/* Funcionalidad para manejo de votaciones con persitencia en POSTGRES DB */
+app.post('/vote', urlencodedParser, function(req, res) {
+  let vote = req.body.yourVote;
+
+  if(vote === 'sandwiches') {
+    votes.sandwiches = votes.sandwiches + 1;
+      client.query('UPDATE votes SET number_of_votes=\'' + votes.sandwiches + '\' WHERE option_name=\'sandwiches\'', function (err, result) {
+        if (err) throw err;
+      });
+  } else if(vote === 'tacos') {
+    votes.tacos = votes.tacos + 1;
+      client.query('UPDATE votes SET number_of_votes=\'' + votes.tacos + '\' WHERE option_name=\'tacos\'', function (err, result) {
+        if (err) throw err;
+      });
+  }  else if(vote === 'otros') {
+    votes.otros = votes.otros + 1;
+      client.query('UPDATE votes SET number_of_votes=\'' + votes.otros + '\' WHERE option_name=\'otros\'', function (err, result) {
+        if (err) throw err;
+      });
+  } else {
+    console.log('Something went wrong: vote contains ' + vote);
+  }
+  res.redirect('/');
+});
 
 const PORT = 8888;
 app.listen(PORT);
